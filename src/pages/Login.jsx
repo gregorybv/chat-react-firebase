60de9e4b7f54482eee6b0a7ab8233687654527f8
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-09 18:05:33 +0300

    add components folder and add the files to it

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from "react"
+// import Add from "../img/addAvatar.png"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+      <div className='formWrapper'>
+        <span className='logo'>GB Chat</span>
+        <span className='title'>Register</span>
+        <form>
+          <input type='email' placeholder='email' />
+          <input type='password' placeholder='password' />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
   )
 }
 
