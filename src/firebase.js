31b2184c6fc76febb0087c9054fb5c53a22da4b5
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-10 21:03:20 +0300

    preparation firebase for registration

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+import { getStorage } from "firebase/storage" // код ниже
 
 const firebaseConfig = {
   apiKey: "AIzaSyBKtJzyyio6uNyHjGBO9QO_CJlpGsKauao",
@@ -7,9 +10,12 @@ const firebaseConfig = {
   projectId: "chat-5a094",
   storageBucket: "chat-5a094.appspot.com",
   messagingSenderId: "969413429360",
-  appId: "1:969413429360:web:00093532cfe762c48e4cb5"
-};
+  appId: "1:969413429360:web:00093532cfe762c48e4cb5",
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
+
+export const storage = getStorage() // берем с сайта firebase и импортируем
+export const db = getFirestore() // не забыть импортировать getFirestore
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,64 @@
-import React from "react"
+import React, { useState } from "react"
 import Add from "../img/addAvatar.png"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth" // с сайта firebase
+import { auth, storage, db } from "../firebase" // добавляем для авторизации
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { doc, setDoc } from "firebase/firestore"
 
 const Register = () => {
+  const [err, setErr] = useState(false) //создаем это для обработки catch(err)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-  
+    // console.log(e.target[0].value) //таким образом мы выведем в консоль name
+
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
+
+    try {
+      //=== этот код взят с сайта firebase
+      // const auth = getAuth() // вместо этого импортируем auth из firebase
+      const res = await createUserWithEmailAndPassword(auth, email, password) //создаем res(удалили код, который был внутри)
+
+      const storageRef = ref(storage, displayName)
+
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      // Register three observers:
+      uploadTask.on(
+        (error) => {
+          setErr(true) // обрабытываем ошибку
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            // updateProfile импортируем из firebase
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            })
+            await setDoc(doc(db, "users", res.user.uid), {
+              // uid берем из devtools chrome
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            })
+          })
+        }
+      )
+    } catch (err) {
+      setErr(true)
+    }
+  }
 
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
         <span className='logo'>GB Chat</span>
         <span className='title'>Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type='text' placeholder='display name' />
           <input type='email' placeholder='email' />
           <input type='password' placeholder='password' />
@@ -20,6 +68,8 @@ const Register = () => {
             <span>Add an avatar</span>
           </label>
           <button>Sign up</button>
+          {/* Обрабатываем ошибку кодом ниже */}
+          {err && <span>Something went wrong</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
