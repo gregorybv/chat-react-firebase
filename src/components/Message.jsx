ae192c0f59a70e31f94102b968be4f9f10222095
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-10 12:29:55 +0300

    add mixin to scss

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react"
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img
+          src="https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'"
+          alt='spider'
+        />
+        <span>just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>hello</p>
+        <img
+          src="https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'"
+          alt='spider'
+        />
+      </div>
+    </div>
   )
 }
 
