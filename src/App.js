70dd90b43a26233c1b93f8564d9504f80920a6cc
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-10 21:14:54 +0300

    add route for path

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import "./style.scss"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
