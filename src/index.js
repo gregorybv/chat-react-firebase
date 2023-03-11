6a3c56153a876c94ea5adc8939eaeeb117e82d20
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-11 13:28:59 +0300

    settings chat component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import { AuthContextProvider } from "./context/AuthContext"
+import { ChatContextProvider } from "./context/ChatContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  // оборачиваем весь проект в AuthContextProvider. 
-  // Этот компонент условно отображает страницу профиля в 
+  // оборачиваем весь проект в AuthContextProvider.
+  // Этот компонент условно отображает страницу профиля в
   // зависимости от статуса аутентификации пользователя
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 )
