d45287ae36136820c15d37ddfcb2a8c53cc5baaf
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-11 12:55:36 +0300

    hello 11.03

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,90 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore"
+import { db } from "../firebase"
+import { AuthContext } from "../context/AuthContext"
 
 const Search = () => {
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState(null)
+  const [err, setErr] = useState(false)
+
+  const { currentUser } = useContext(AuthContext)
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    )
+
+    try {
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      })
+    } catch (err) {
+      setErr(true)
+    }
+  }
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch()
+  }
+
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid
+    try {
+      const res = await getDocs(db, "chats", combinedId)
+
+      if (!res.exists()) {
+        // создаем чат в коллекции чатов
+        await setDoc(doc(db, "chats", combinedId), { messages: [] })
+
+        // создаем user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoUrl: user.photoUrl,
+          },
+          [combinedId + ".date"] : serverTimestamp()
+        })
+      }
+    } catch (err) {}
+  }
+
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='Find a user' />
-      </div>
-      <div className='userChat'>
-        <img
-          src='https://images.unsplash.com/photo-1534809027769-b00d750a6bac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-          alt='/'
+        <input
+          type='text'
+          placeholder='Find a user'
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <div className='userChatInfo'>
-          <span>Spider</span>
-        </div>
       </div>
+      {/* обработка ошибки */}
+      {err && <span>User not found!</span>}
+      {/* обработка ошибки */}
+      {user && (
+        <div className='userChat' onClick={handleSelect}>
+          <img src={user.photoUrl} alt='/' />
+          <div className='userChatInfo'>
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
